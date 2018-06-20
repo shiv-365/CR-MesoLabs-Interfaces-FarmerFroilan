88b928adeb311e6c425ff438d320a8e49d85a534
Shivam Patel <shivamp@shivam.local>
2018-06-20 14:43:58 -0400

    person complete

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Person.java
@@ -0,0 +1,21 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Person /*implements Eater, NoiseMaker*/{
+   private String name;
+
+
+    public Person(String name){
+        this.name = name;
+
+    }
+    public void eat(Object e){ // change to Edible e
+
+
+    }
+    public String makeNoise(){
+
+        return null;
+    }
+
+
+}
