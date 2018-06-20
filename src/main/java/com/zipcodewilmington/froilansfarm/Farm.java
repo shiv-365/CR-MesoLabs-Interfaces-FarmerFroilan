1639fe51eb056088f42a58214fefc955d939d161
Ben Blinebury <BenB@Ben.local>
2018-06-20 16:38:34 -0400

    farm

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm.java
@@ -0,0 +1,10 @@
+package com.zipcodewilmington.froilansfarm;
+
+import java.util.ArrayList;
+
+public class Farm {
+    FarmHouse saison = new FarmHouse();
+    TreeMap<crop, cropRow>allTheVeggies = new TreeMap<crop, cropRow>();
+    ArrayList<stable> horseyFarm = new ArrayList<stable>();
+    ArrayList<chickenCoop> chickenCity = new ArrayList<chickenCoop>();
+}
