344a4970fabb87d01ef0d09f0f5620e10ee4d401
Vincent Sima <vincesima@gmail.com>
2018-06-21 15:54:47 -0400

    tests

--- src/test/java/com/zipcodewilmington/froilansfarm/Farm/FarmhouseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Farm/FarmhouseTest.java
@@ -1,5 +1,38 @@
+package com.zipcodewilmington.froilansfarm.Farm;
+
+import com.zipcodewilmington.froilansfarm.Persons.Person;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FarmhouseTest {
 
+    @org.junit.jupiter.api.Test
+    void getPerson() {
+        Farmhouse f = new Farmhouse();
+        Person p = new Person("person1");
+        f.setPerson(p);
+        Object actual = f.getPerson(0);
+        Object expected = p;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPerson() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePerson() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEdible() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEdible() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeEdible() {
+    }
 }
