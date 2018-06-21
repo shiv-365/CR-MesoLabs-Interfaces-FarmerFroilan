23ef157401e4de38a19e25c57f1a101795e8865f
Vincent Sima <vincesima@gmail.com>
2018-06-21 16:22:32 -0400

    tests

--- src/test/java/com/zipcodewilmington/froilansfarm/Farm/FarmhouseTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Farm/FarmhouseTest.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.froilansfarm.Farm;
 
 import com.zipcodewilmington.froilansfarm.Persons.Person;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FarmhouseTest {
@@ -9,15 +10,19 @@ class FarmhouseTest {
     @org.junit.jupiter.api.Test
     void getPerson() {
         Farmhouse f = new Farmhouse();
-        Person p = new Person("person1");
-        f.setPerson(p);
         Object actual = f.getPerson(0);
-        Object expected = p;
-        assertEquals(expected, actual);
+        assertTrue(actual instanceof Person);
     }
 
     @org.junit.jupiter.api.Test
     void setPerson() {
+        Farmhouse f = new Farmhouse();
+        Person p = new Person("person1");
+        f.setPerson(p);
+        Object actual = f.getPerson(2);
+        Object expected = p;
+        assertEquals(expected, actual);
+
     }
 
     @org.junit.jupiter.api.Test
