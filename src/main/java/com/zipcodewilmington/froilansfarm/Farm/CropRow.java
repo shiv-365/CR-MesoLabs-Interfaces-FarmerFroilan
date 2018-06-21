498846d512bac91841d92c4062d4e7ae9fbcc7a7
David Thornley <davidT@Davids-zipcode-MacBook.local>
2018-06-21 14:03:23 -0400

    stuff

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Farm/CropRow.java
@@ -0,0 +1,26 @@
+package com.zipcodewilmington.froilansfarm.Farm;
+
+import com.zipcodewilmington.froilansfarm.crops.Crop;
+
+import java.util.ArrayList;
+
+public class CropRow {
+
+  ArrayList<Crop> cropRow;
+
+    public CropRow(ArrayList<Crop> cropRow) {
+        this.cropRow = cropRow;
+    }
+
+    public ArrayList<Crop> getCropRow() {
+        return cropRow;
+    }
+
+    public void setCropRow(ArrayList<Crop> cropRow) {
+        this.cropRow = cropRow;
+    }
+
+    public Object[] crops(){
+        return cropRow.toArray();
+    }
+}
--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Tractor.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
+import com.zipcodewilmington.froilansfarm.Farm.CropRow;
 import com.zipcodewilmington.froilansfarm.Interfaces.Edible;
 import com.zipcodewilmington.froilansfarm.Interfaces.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.crops.Crop;
@@ -15,26 +16,28 @@ public class Tractor extends Vehicle implements FarmVehicle {
         this.operate = false;
     }
 
-    public void operate(){
+    public void operate() {
         this.operate = true;
     }
 
-    public String makeNoise(){
+    public String makeNoise() {
         return "VROOOMMMM VRRRRROOOOOOMMMMM";
     }
 
-    public void ridden(){
+    public void ridden() {
         super.isRidden = true;
     }
 
-//    public ArrayList<Edible> harvest(/*CropRow cropRow*/){
-//        ArrayList<Edible> theHarvest = new ArrayList<Edible>();
-//
-//        for(Crop c: /*cropRow*/){
-//            if(c instanceof Edible){
-//                theHarvest.add(c);
-//            }
-//        }
-//        return theHarvest;
+    public ArrayList<Edible> harvest(CropRow cropRow) {
+        ArrayList<Edible> theHarvest = new ArrayList<Edible>();
+
+
+        for (Crop c : cropRow) {
+            if (c instanceof Edible) {
+                theHarvest.add(c);
+            }
+        }
+        return theHarvest;
     }
+}
 
